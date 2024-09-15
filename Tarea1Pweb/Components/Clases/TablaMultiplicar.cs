a63e8e14916daf65a6d0c3351795fefd8b3d07a9
Jenry Sanchez <henryfuerte560@gmail.com>
2024-09-14 23:20:33 -0400

    ultimo comit de la la tarea

---
+++ Tarea1Pweb/Components/Clases/TablaMultiplicar.cs
@@ -0,0 +1,9 @@
+
+namespace Tarea1Pweb.Components.Clases
+{
+    public class Multiplicar
+    {
+        public string? expresion { get; set; }
+        public int? resultado { get; set; }
+    }
+}
